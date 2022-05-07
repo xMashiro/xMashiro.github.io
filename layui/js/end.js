bee3d274242b29a51133d96cb7a5f05a5f7359bd
xMashiro <77237653+xMashiro@users.noreply.github.com>
2022-05-07 22:08:03 +0800

    Add files via upload

---
+++ layui/js/end.js
@@ -0,0 +1,13 @@
+function outDiv(){
+			 document.getElementById("div1").style.display="block";
+			 document.getElementById("div2").style.display="none";
+			 document.getElementById("div3").style.display="block";
+			 
+			}
+			function outDiv2(){
+				
+			document.getElementById("div1").style.display="none";
+			document.getElementById("div2").style.display="block";				
+			document.getElementById("div3").style.display="none";
+								
+			}
